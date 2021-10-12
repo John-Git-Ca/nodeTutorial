f1ba031fc8848cffd73161e757b70b6b20027dbd
John Li <john.lizuhua@outlook.com>
2021-10-11 21:32:45 -0400

    asyn

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
-const _ = require("lodash")
+const { log } = require("console")
+const http = require("http")
 
-const items = [1, [2, [3, [4]]]]
+const server = http.createServer((req, res)=>{
+    if(req.url === "/"){
+        res.end("Home page")
+    }
+    if(req.url === "/about"){
+        for(let i = 0; i < 1000; i++){
+            for(let j = 0; j < 1000; j++){
+                console.log(`${i} ${j}`);
+            }
+        }
+        res.end("about page")
+    }
+    res.end("error page")
+})
 
-const newItems = _.flattenDeep(items)
-
-console.log(newItems);
+server.listen(5000, ()=>{
+    console.log("server is listening on port 5000...");
+})
